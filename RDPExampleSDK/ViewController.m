6d3060646ef23e849becb0dfd78212f6f31cd2c5
SimbaHJ <545119722@qq.com>
2018-03-13 13:31:08 +0800

    Initial Commit

---
+++ RDPExampleSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RDPExampleSDK
+//
+//  Created by 霍驹 on 2018/3/13.
+//  Copyright © 2018年 HJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
